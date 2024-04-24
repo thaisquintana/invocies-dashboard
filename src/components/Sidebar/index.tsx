97b55d7f0323066bb7db6b4841262c7d20e5df9f
Thais Quintana <thaisquintanap@gmail.com>
2024-04-24 02:38:16 -0300

    added sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,15 @@
+export const Sidebar: React.FC = () => {
+    return (
+        <div className="h-auto p-8 w-full bg-slate-300">
+            <ul>
+                <li>Item A</li>
+                <li>Item B</li>
+                <li>Item C</li>
+                <li>Item D</li>
+                <li>Item E</li>
+                <li>Item F</li>
+                <li>Item G</li>
+            </ul>
+        </div>
+    )
+}
