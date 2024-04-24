c794d377aa43671e1fdf486390fdb530b01af217
Thais Quintana <thaisquintanap@gmail.com>
2024-04-24 02:00:51 -0300

    added header and footer components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export const Footer: React.FC = () => {
+    return (
+        <footer className="bg-midnight h-20 my-0 mx-auto flex justify-center items-center flex-col text-white">
+            <div>Aqui é o footer</div>
+        </footer>
+    )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header: React.FC = () => {
+    return (
+        <header className="bg-midnight h-20 my-0 mx-auto flex justify-center items-center flex-col text-white">
+            <div>Aqui é o header</div>
+        </header>
+    )
+}
