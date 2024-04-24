13854d5fb261e6ccf58337d90bf10e7a4bc7fc5a
Thais Quintana <thaisquintanap@gmail.com>
2024-04-24 02:58:44 -0300

    added card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,10 @@
+export const Card: React.FC = () => {
+  return (
+    <div className="w-full p-4 bg-slate-500 h-auto rounded-lg">
+      <p>Card</p>
+      <label>Um novo valor</label>
+      <br />
+      <label>Every single day!</label>
+    </div>
+  );
+};
