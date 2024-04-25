9c89ed13ed2cfbc9d3b6ca5f7094ce4176874877
Thais Quintana <thaisquintanap@gmail.com>
2024-04-25 02:52:50 -0300

    added sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,15 +1,27 @@
+import { HomeIcon, UserRoundCog, UserRoundMinus, UserRoundPlus } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export const Sidebar: React.FC = () => {
-    return (
-        <div className="h-auto p-8 w-full bg-slate-100 border border-slate-400">
-            <ul>
-                <li>Item A</li>
-                <li>Item B</li>
-                <li>Item C</li>
-                <li>Item D</li>
-                <li>Item E</li>
-                <li>Item F</li>
-                <li>Item G</li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="p-8 w-full bg-slate-100 border border-slate-400">
+      <ul>
+        <li className="flex items-center gap-3 mb-4">
+            <HomeIcon size={16} color="#d8168a"/>
+          <Link to="/home" className="text-taupe-gray font-semibold">Início</Link>
+        </li>
+        <li className="flex items-center gap-3 mb-4">
+          <UserRoundPlus size={16} color="#d8168a"/>
+          <Link to="/register-new-user" className="text-taupe-gray font-semibold">Cadastrar assinante</Link>
+        </li>
+        <li className="flex items-center gap-3 mb-4">
+            <UserRoundCog size={16} color="#d8168a"/>
+          <Link to="/edit-user-subscription" className="text-taupe-gray font-semibold">Editar assinatura</Link>
+        </li>
+        <li className="flex items-center gap-3 mb-4">
+            <UserRoundMinus size={16} color="#d8168a"/>
+          <Link to="/cancel-user-subscription" className="text-taupe-gray font-semibold">Cancelar assinatura</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
