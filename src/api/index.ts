163e36120bed3ca83f606a8eca35f23363060aae
Thais Quintana <thaisquintanap@gmail.com>
2024-05-02 00:59:34 -0300

    integrate with API

---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export const baseURL = import.meta.env.VITE_FRONT_END_GRAPHQL_ENDPOINT as string
+
+export const api = axios.create({
+    baseURL: baseURL 
+});
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,44 +9,71 @@ import {
 import { Card } from "../../components/Card";
 import { Table } from "../../components/Table";
 import { columns, rows } from "../../constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Search } from "../../components/Search";
 import { Link, useNavigate } from "react-router-dom";
 import { useToast } from "../../hooks/useToast";
+import { api } from "../../api";
 
 export const Home: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  // const pageSize = Number(entity?.userCount) >= 60 ? 60 : 10
-  const pageSize = 10 >= 60 ? 60 : 10;
-  const navigate = useNavigate()
-  const { toastMessage } = useToast()
+  const [subscriptions, setSubscriptions] = useState([]);
+  const navigate = useNavigate();
+  const { toastMessage } = useToast();
 
-  const handleDelete = (id: number) => {
-    console.log(id);
-    toastMessage({
-      message: 'Assinatura cancelada com sucesso.',
-      type: 'success',
-      position: "bottom-right"
-    })
+  const getSubscriptions = async () => {
+    const response = await api.get("/subscriptions");
+    setSubscriptions(response.data);
   };
+
+  const handleDelete = async (id: string) => {
+    api
+      .get(`/subscriptions/${id}`)
+      .then((response) => {
+        api.put(`/subscriptions/${id}`, {
+          ...response.data[0],
+          status: "Cancelado"
+        })
+        toastMessage({
+          message: "Assinatura cancelada com sucesso.",
+          type: "success",
+          position: "bottom-right",
+        });
+        getSubscriptions()
+      })
+      .catch((err) => {
+        console.log(err);
+        toastMessage({
+          message: "Erro ao excluir cadastro.",
+          type: "error",
+          position: "bottom-right",
+        });
+      });
+  };
+
+  const filterByActiveSubscriptions = async () => {
+    const response = await api.get("/subscriptions?search=ativo");
+    setSubscriptions(response.data);
+  };
+
+  const filterByCancelSubscriptions = async () => {
+    const response = await api.get("/subscriptions?search=cancelado");
+    if (response.data.lenght === 0) {
+      return setSubscriptions([]);
+    } else {
+      return setSubscriptions(response.data);
+    }
+  };
+
+  useEffect(() => {
+    if(subscriptions.length === 0) {
+      getSubscriptions()
+    }
+  }, []);
 
   const redirectAddNewSubscription = () => {
-    navigate('/register-new-user')
+    navigate("/register-new-user");
   };
-
-  const prevPageList = () => {
-    const prevPage = currentPage - 1;
-    setCurrentPage(prevPage);
-  };
-
-  const nextPageList = () => {
-    const nextPage = currentPage + 1;
-    setCurrentPage(nextPage);
-  };
-  //   const userCount = Number(entity?.userCount)
-  const userCount = 1;
-  const pageSizeCount =
-    userCount >= 60 ? Math.ceil(userCount / 60) : Math.ceil(userCount / 10);
 
   return (
     <div className="p-8 h-screen">
@@ -96,13 +123,27 @@ export const Home: React.FC = () => {
       </div>
       <div className="my-4 flex justify-end">
         <div className="mr-2">
-          <button className="bg-white border rounded-lg border-weldon-blue p-3 text-deep-space-sparkle  hover:text-warm-black hover:border-warm-black hover:bg-ghost-writer">
+          <button
+            className="bg-white border rounded-lg border-weldon-blue p-3 text-deep-space-sparkle  hover:text-warm-black hover:border-warm-black hover:bg-ghost-writer"
+            onClick={() => getSubscriptions()}
+          >
+            Mostrar todos
+          </button>
+        </div>
+        <div className="mr-2">
+          <button
+            className="bg-white border rounded-lg border-weldon-blue p-3 text-deep-space-sparkle  hover:text-warm-black hover:border-warm-black hover:bg-ghost-writer"
+            onClick={() => filterByActiveSubscriptions()}
+          >
             Mostrar ativos
           </button>
         </div>
         <div className="mr-2">
-          <button className="bg-white border rounded-lg border-weldon-blue p-3 text-deep-space-sparkle hover:text-warm-black hover:border-warm-black hover:bg-ghost-writer">
-            Mostrar inativos
+          <button
+            className="bg-white border rounded-lg border-weldon-blue p-3 text-deep-space-sparkle hover:text-warm-black hover:border-warm-black hover:bg-ghost-writer"
+            onClick={() => filterByCancelSubscriptions()}
+          >
+            Mostrar cancelados
           </button>
         </div>
         <div className="mr-2">
@@ -114,32 +155,31 @@ export const Home: React.FC = () => {
           </button>
         </div>
       </div>
-      <Table
-        columns={columns}
-        data={rows}
-        perPage={pageSize}
-        currentPage={currentPage}
-        totalData={userCount}
-        nextPageList={() => {
-          nextPageList();
-        }}
-        prevPageList={() => {
-          prevPageList();
-        }}
-        pageCount={pageSizeCount}
-        buttonActions={(id: number) => (
-          <div className="flex gap-8">
-            <div className="cursor-pointer">
-              <Link to={`/edit-user-subscription/${id}`}>
-                <PencilIcon className="hoverIcon" />
-              </Link>
+      {subscriptions.length ? (
+        <Table
+          columns={columns}
+          data={subscriptions}
+          buttonActions={(id: number) => (
+            <div className="flex gap-8">
+              <div className="cursor-pointer">
+                <Link to={`/edit-user-subscription/${id}`}>
+                  <PencilIcon className="hoverIcon" />
+                </Link>
+              </div>
+              <div
+                className="cursor-pointer"
+                onClick={() => handleDelete(String(id))}
+              >
+                <Trash2Icon className="hoverIcon" />
+              </div>
             </div>
-            <div className="cursor-pointer" onClick={() => handleDelete(id)}>
-              <Trash2Icon className="hoverIcon" />
-            </div>
-          </div>
-        )}
-      />
+          )}
+        />
+      ) : (
+        <div className="flex justify-center my-20">
+          <p className="text-gray-600">Não há dados para mostrar.</p>
+        </div>
+      )}
     </div>
   );
 };
